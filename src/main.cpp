d56ee42ea1328019e61606ac1fb07d0b60b5c466
Aakarshan Raj <aakarshan7raj@gmail.com>
2023-02-06 17:56:16 +0530

    first block, but cannot render on floating Co-ordinates

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 
 #include <SDL2/SDL.h>
 #include <iostream>
-#define WIDTH 1200
+#define WIDTH 900
 #define HEIGHT 900
 
 int main(){
@@ -9,23 +9,33 @@ int main(){
 SDL_Window* window;
 SDL_Surface* gsurface;
 SDL_Event e;
+SDL_Renderer* grender;
+
+
+SDL_Rect box_rec[64];
+SDL_Rect r = {0,0,112,112};
+
 
 if(SDL_Init(SDL_INIT_EVERYTHING)<0){
-  std::cout<<"SDL Init error"<<std::endl;
+  std::cout<<"SDL Initialisation error"<<std::endl;
 }
 
 else{
 window = SDL_CreateWindow("Chess",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
 bool quit = false;
+grender = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+
 while(!quit){
     while(SDL_PollEvent(&e) == 1){
         if(e.type == SDL_QUIT){
             quit = true;
         }
     }
-  
-    SDL_UpdateWindowSurface(window);
-    
+    SDL_SetRenderDrawColor(grender,0xff,0xff,0xff,0xff);
+    SDL_RenderClear(grender);
+    SDL_SetRenderDrawColor(grender,0xff,0xc0,0xee,0xff);
+    SDL_RenderFillRect(grender,&r);
+    SDL_RenderPresent(grender);    
 }
 
 }
